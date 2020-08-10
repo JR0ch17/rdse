00443a5a83abff5a763be713767415e304969541
root <jasmin.landry@hotmail.com>
2020-08-10 06:41:14 +0000

    Added subdomain check with isValidDomain

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const isValidDomain = require('is-valid-domain')
 let domain = process.argv[2];
 let subdomains = [];
 
-if (isValidDomain(domain)) {
+if (isValidDomain(domain) || isValidDomain(subdomain)) {
   axios.get(`https://api.recon.dev/search?domain=${domain}`)
     .then(function (response) {
       response.data.map(subdomain => {
