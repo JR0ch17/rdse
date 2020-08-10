94a664bf80f2226ba8f9c629773cf8dbe841a1a0
root <jasmin.landry@hotmail.com>
2020-08-10 06:29:25 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const isValidDomain = require('is-valid-domain')
+
+let domain = process.argv[2];
+let subdomains = [];
+
+if (isValidDomain(domain)) {
+  axios.get(`https://api.recon.dev/search?domain=${domain}`)
+    .then(function (response) {
+      response.data.map(subdomain => {
+        subdomain.domain = subdomain.domain.replace('https://', '').split(':')[0];
+        subdomains.push(subdomain.domain);
+      });
+      subdomains = [...new Set(subdomains)];
+      console.log(subdomains.sort());
+    })
+    .catch(function (error) {
+      console.log(`Could not grab subdomains: ${error}`);
+    })
+    .finally(function () {
+    });
+} else {
+  console.log("Incorrect domain name syntax")
+}
